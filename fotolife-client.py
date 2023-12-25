8212cb6c596f5aade6cb49ebc7976c4d4adc07e4
Furutsuki <furutsuki@hatena.ne.jp>
2023-12-25 15:44:56 +0900

    fotolife-client.py の修正
    title, altへの対応とコードの整形

--- fotolife-client.py
+++ fotolife-client.py
@@ -8,7 +8,7 @@ import secrets
 import mimetypes
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
-import xml.etree.ElementTree as ElementTree
+from xml.etree import ElementTree
 
 ATOM_TEMPLATE = """
 POST /atom/post
@@ -20,7 +20,13 @@ POST /atom/post
 </entry>
 """
 
+wsse_value = ""
+base_dir = ""
+
 def wsse(username, api_key):
+    """
+    ユーザのIDとAPIキーからX-WSSEヘッダの値を生成する
+    """
     nonce = sha1(secrets.token_bytes(16)).digest()
     now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + "Z"
     digest = sha1(nonce + now.encode() + api_key.encode()).digest()
@@ -31,57 +37,92 @@ def wsse(username, api_key):
         now,
     )
 
-def upload_to_fotolife(match):
-    alt, path, _, _ = match.groups()
-
-    # pathがローカルのファイルを指していなければ何もしない
-    path = os.path.join(base_dir, path)
-    if not os.path.exists(path):
-        return match[0]
-
+def upload_to_fotolife(path: str) -> str|None:
+    """
+    画像のパスを受け取って、その画像ファイルをFotolifeにアップロードする
+    その画像をFotolife記法として埋め込むための文字列を返す。
+    """
     mime = mimetypes.guess_type(path)[0]
     with open(path, "rb") as f:
         imgbuf = f.read()
 
     body = ATOM_TEMPLATE.format(mime, b64encode(imgbuf).decode())
     req = Request("https://f.hatena.ne.jp/atom/post", method="POST", headers={
-        "X-WSSE": wsseValue,
+        "X-WSSE": wsse_value,
     }, data=body.encode())
     try:
         with urlopen(req) as res:
             if not 200 <= res.status < 300:
-                print("[-] failed to request: {}, reason: {}".format(res.url, res.reason))
-                return match[0]
+                print(f"[-] failed to request: {res.url}, reason: {res.reason}")
+                return None
 
             resbuf = res.read()
             tree = ElementTree.fromstring(resbuf)
             ns = {
                 "hatena": "http://www.hatena.ne.jp/info/xmlns#"
             }
-            urls = tree.findall("hatena:syntax", ns)
-            url = urls[0].text
-            print("[+] uploaded {}".format(path))
+            syntaxes = tree.findall("hatena:syntax", ns)
+            syntax = syntaxes[0].text
+            print(f"[+] uploaded {path}")
+            return syntax
     except HTTPError as e:
-        print(e)
+        print(f"[-] failed to request: {e.url}, reason: {e.reason}")
+        return None
+
+
+def replace_to_fotolife_syntax(match):
+    """
+    Markdownの画像記法で参照されている画像をFotolifeにアップロードしつつ、Fotolife記法に置き換える
+    """
+    alt = match.group("alt")
+    path = match.group("path")
+    title = match.group("title")
+
+    # pathがローカルのファイルを指していなければ何もしない
+    path = os.path.join(base_dir, path)
+    if not os.path.exists(path):
         return match[0]
 
+    syntax = upload_to_fotolife(path)
+    if syntax is None:
+        return match[0]
 
-    if not alt:
-        return "[{}]".format(url)
-    else:
-        return "[{}:title={}]".format(url, alt)
+    if alt and title:
+        return f"[{syntax}:title={title}:alt={alt}]"
+    elif (not alt) and title:
+        return f"[{syntax}:title={title}]"
+    elif alt and (not title):
+        return f"[{syntax}:alt={alt}]"
+    elif (not alt) and (not title):
+        return f"[{syntax}]"
 
-if len(sys.argv) == 1:
-    print("Usage: {} <file>".format(sys.argv[0]))
-    exit(1)
+def main():
+    """
+    usage: python3 fotolife-client.py <file>
+    """
+    global wsse_value
+    global base_dir
 
-wsseValue = wsse(os.getenv("HATENA_ID"), os.getenv("OWNER_API_KEY"))
 
-target = sys.argv[1]
-buf = open(target, "r").read()
-base_dir = os.path.dirname(target)
-pattern = re.compile(r"""!\[([^\]]*)\]\(([^\)]*)\s*(?:"([^"]*)"\s*)?(?:"([^"]*)"\s*)?\)""")
+    hatena_id = os.getenv("HATENA_ID")
+    owner_api_key = os.getenv("OWNER_API_KEY")
+    if not hatena_id or not owner_api_key:
+        print("please set environment variables: HATENA_ID, OWNER_API_KEY")
+        sys.exit(1)
+    wsse_value = wsse(hatena_id, owner_api_key)
 
-res = pattern.sub(upload_to_fotolife, buf)
-with open(target, "w") as f:
-    f.write(res)
+    if len(sys.argv) == 1:
+        print(f"usage: python3 {sys.argv[0]} <file>")
+        sys.exit(1)
+    target = sys.argv[1]
+    with open(target, "r", encoding="utf-8") as f:
+        buf = f.read()
+    base_dir = os.path.dirname(target)
+
+    pattern = re.compile(r"""!\[(?P<alt>[^\]]*)\]\((?P<path>[^\)]*)\s*(?P<title>"(?:[^"]*)"\s*)\)""")
+    res = pattern.sub(upload_to_fotolife, buf)
+    with open(target, "w", encoding="utf-8") as f:
+        f.write(res)
+
+if __name__ == "__main__":
+    main()
